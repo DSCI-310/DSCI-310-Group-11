cbadba703ea690eb6ff6ce5471eaec5efacc279f
samzzzzzh <98234153+samzzzzzh@users.noreply.github.com>
2022-03-11 20:22:36 -0800

    Update tests/testthat/helper-data_cleaning.R
    Co-authored-by: a-kong <amykong26@gmail.com>

--- tests/testthat/helper-data_cleaning.R
+++ tests/testthat/helper-data_cleaning.R
@@ -3,5 +3,5 @@ gender <- c("male", "male", 'female')
 num <- c(1:3)
 
 test_data <- data.frame(people, gender, num)
-test_data_two_columns <- data.frame(gender,num)
+test_data_two_columns <- data.frame(gender, num)
 test_data_one_column <- data.frame(gender)
