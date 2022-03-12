cf49207da973502c54956be63f38fe77f7b0c320
Amy Kong <amykong26@gmail.com>
2022-03-12 01:11:38 -0800

    removed knn_spec abstraction
    function does not work as intended

--- src/R/correlation_graph.R
+++ src/R/correlation_graph.R
@@ -1,19 +1,17 @@
-#' scatter2d 
-#' A short-cut function for creating 2 dimensional ggpair scatterplots via GGally and ggplot2
-#'
+#' Create ggpair scatter plot
+#' 
+#' A short-cut function for creating 2 dimensional ggpair scatter plots
+#' 
 #' @param data A data frame or tibble.
-#' @param title title of the plot from the data frame or tibble 
+#' @param title title of the plot from the data frame or tibble
 #'
-#' @return returns a ggpairs scatterplot representing a correlation matrix between pairs of variables
+#' @return returns a ggpairs scatter plot with correlation values between pairs of variables
 #' @export
 #'
 #' @examples
 #' scatter2d(mtcars, hp, mpg)
-
-correlation_graph <- function (.data, gg_title) {
-  GGally::ggpairs(data = .data, 
-                  ggplot2::aes(colour = "steelblue", alpha = 0.5)) + 
+correlation_graph <- function(.data, gg_title) {
+  GGally::ggpairs(data = .data,
+                  ggplot2::aes(colour = "steelblue", alpha = 0.5)) +
     ggplot2::ggtitle(gg_title)
-
-
-
+}
