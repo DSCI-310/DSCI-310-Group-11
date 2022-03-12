e7b2177c5df06ce4efaff602d1a0f806759affe7
Sam Zhang <huym2000@gmail.com>
2022-03-11 22:02:15 -0800

    changed the directory of the files

---
+++ src/R/.ipynb_checkpoints/data_cleaning-checkpoint.R
@@ -0,0 +1,25 @@
+#' Creates a new data frame with selected columns dropped from the old data frame
+#'
+#' @param dataFrame A data frame or data frame extension (e.g. a tibble).
+#' @param columns Name(s) of column(s) from the dataFrame 
+#'
+#' @return A new data frame with columns dropped from the old data frame
+#'
+#' @export
+#'
+#' @examples
+#' data_cleaning(fire_data, c("X","Y","month"))
+data_cleaning <- function(dataFrame, columns){
+  if (!is.data.frame(dataFrame)) {
+    stop("DataFrame must be a data frame! Please try again.")
+  }
+  colNames = names(dataFrame)
+  for (c in columns) {
+    if ({c} %in% colNames) {
+      dataFrame = dataFrame |> dplyr::select(-{c})
+    } else {
+      stop("Wrong column names! Please try again")
+    }
+  }
+  return(dataFrame)
+}             
