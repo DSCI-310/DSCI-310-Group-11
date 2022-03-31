355087c444a20af54178634596a0c895ea6ef0f3
Amy Kong <amykong26@gmail.com>
2022-03-31 12:18:02 -0700

    removed colouring from plots

--- src/R/Correlation_matrix_plot.R
+++ src/R/Correlation_matrix_plot.R
@@ -2,9 +2,9 @@
 #date: 2022-03-30
 
 "Creates a ggpairs plot for the trained data from the Forest Fire Dataset.
-Saves the plot as a png and pdf file
+Saves the plot as a png.
 
-Usage: src/Correlation_matrix_plot.R <train> <out_dir>
+Usage: src/R/Correlation_matrix_plot.R <train> <out_dir>
 " -> doc
 
 library(tidyverse)
@@ -15,16 +15,14 @@ library(docopt)
 
 opt <- docopt(doc)
 
-
 trained_data <- read_csv(opt$train, show_col_types = FALSE)
 
-
 create_plot <- trained_data %>%
-  ggpairs(aes(colour = "steelblue", alpha = 0.5))
+  ggpairs(aes(alpha = 0.5))
 
 create_plot_selected <- trained_data %>% 
   select(DC, DMC, ISI, FFMC, temp, RH) %>% 
-  ggpairs(aes(colour = "steelblue", alpha = 0.5))
+  ggpairs(aes(alpha = 0.5))
     
   
 ggsave(paste0(opt$out_dir, "/correlation_graph.png"),
